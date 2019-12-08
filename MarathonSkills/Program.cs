1e6bf2b14ae8772c8e669c609de2bc280ef16afc
Stepan <sssprogramer@gmail.com>
2019-12-08 13:34:11 +0300

    rename project

--- MarathonSkills/Program.cs
+++ MarathonSkills/Program.cs
@@ -2,17 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MarathonSkills.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Marathon
+namespace MarathonSkills
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            DbContextOptionsBuilder<MarathonContext> builder = new DbContextOptionsBuilder<MarathonContext>();
+            MarathonContext context = new MarathonContext(builder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MarathonDB;Trusted_Connection=True;").Options);
+            context.Database.EnsureCreated();
             CreateHostBuilder(args).Build().Run();
         }
 
--- MarathonSkills/Startup.cs
+++ MarathonSkills/Startup.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Marathon.Models;
+using MarathonSkills.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,7 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Marathon
+namespace MarathonSkills
 {
     public class Startup
     {
