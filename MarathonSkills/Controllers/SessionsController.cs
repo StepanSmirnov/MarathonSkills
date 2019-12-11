bca11c4c89d7583219f0781cb0a7779a8bb48400
Stepan <sssprogramer@gmail.com>
2019-12-11 22:28:49 +0300

    add login page

---
+++ MarathonSkills/Controllers/SessionsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MarathonSkills.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using MarathonSkills.Models;
+
+namespace MarathonSkills.Controllers
+{
+    public class SessionsController : Controller
+    {
+        private readonly SignInManager<User> signInManager;
+        private readonly UserManager<User> userManager;
+
+        public SessionsController(SignInManager<User> signInManager, UserManager<User> userManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> CreateAsync(SessionViewModel model)
+        {
+            //var findResult = await userManager.FindByIdAsync(model.Email);
+            var singInResult = await signInManager.PasswordSignInAsync(model.Email, model.Password, false, false) ;
+            if (singInResult.Succeeded)
+            {
+                return View();
+            }
+            
+            return View();
+        }
+
+        public IActionResult Destroy()
+        {
+            return View();
+        }
+    }
+}
